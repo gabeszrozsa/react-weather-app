c63ada24327f0edd247203e211edf45e07da8c8f
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-02-06 20:31:24 +0100

    add pages :rocket:

--- app/app.jsx
+++ app/app.jsx
@@ -3,11 +3,16 @@ const ReactDOM = require('react-dom');
 const { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 const Main = require('Main');
+const Weather = require('Weather');
+const About = require('About');
+const Examples = require('Examples');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-
+      <Route path="about" component={About}></Route>
+      <Route path="examples" component={Examples}></Route>
+      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,9 +1,15 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 const Nav = React.createClass({
   render: function() {
     return (
-      <h2>Nav Component</h2>
+      <div>
+        <h3>Nav Component</h3>
+        <Link to="/">Get Weather</Link>
+        <Link to="/about">About</Link>
+        <Link to="/examples">Examples</Link>
+      </div>
     );
   }
 });
